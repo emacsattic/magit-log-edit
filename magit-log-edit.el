551ee1f595edba38280ca94cbf8e3ad85babf1e7
Jonas Bernoulli <jonas@bernoul.li>
2015-02-01 14:26:57 +0100

    this is obsolete now, no longer just semi-obsolete

--- magit-log-edit.el
+++ magit-log-edit.el
@@ -1,4 +1,4 @@
-;;; magit-log-edit.el --- [SEMI-OBSOLETE] major mode for editing Git commit messages
+;;; magit-log-edit.el --- [OBSOLETE use at your own risk] major mode for editing Git commit messages
 
 ;; Copyright (C) 2008-2013  The Magit Project Developers.
 ;;
@@ -26,26 +26,14 @@
 
 ;;; Commentary:
 
-;;     ****   THIS PACKAGE IS SEMI-OBSOLETE.   ****
+;;     ****      THIS PACKAGE IS OBSOLETE.     ****
 ;;     **** YOU PROBABLY DON'T WANT TO USE IT. ****
 
 ;; A major mode for editing Git commit messages.
 
 ;; This is the mode that was used before Magit switched to using the
-;; external `git-commit-mode'.  That still has some problems, though:
-
-;; Well, actually only one is left:
-
-;; - Doesn't work well with tramp.
-;;   https://github.com/magit/magit/issues/801
-
-;; Both issues will be addressed, but for some people these issues are
-;; deal breakers, and so I have this old package available again.
-
-;; If you are not actually bothered much by these issue (or did not
-;; even notice until you read this) then DO NOT install this package.
-;; It also has its limitiations, which in least in my eyes are much
-;; worse.
+;; external `git-commit-mode'.  All issues that library used to have
+;; have long been fixed.
 
 ;;; Code:
 
